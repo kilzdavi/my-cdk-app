951f8122392f5cd4e73c3b66044ce9c4d45b6713
kilzdavi <kilzdavi@amazon.com>
2023-09-17 22:51:19 +0000

    Initial commit

---
+++ src/MyCdkApp/MyCdkAppStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace MyCdkApp
+{
+    public class MyCdkAppStack : Stack
+    {
+        internal MyCdkAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
